5d0b019b12e1506c9e22b3d86815d321823c3a75
louloulfx <louloulfx1@hotmail.fr>
2019-03-27 14:26:46 +0100

    Affichage de la liste

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <show-list></show-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import showList from './components/showList.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    'show-list': showList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/showList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="show-list">
+    <h1>Voici la liste des fournisseurs</h1>
+    <div v-for="provider in providers" :key="provider" class="single-blog">
+      <h2>{{ provider.name }}</h2>
+      <article>{{ provider.description }}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      providers: []
+    }
+  },
+  methods: {
+
+  },
+  created(){
+    this.$http.get('https://damp-hollows-18655.herokuapp.com/provider').then(function(data){
+      this.providers = data.body;
+    })
+  }
+}
+</script>
+
+<style scoped>
+</style>
