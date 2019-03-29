b1688e71caa51a58a00db379ada39b041c22aab5
Delreux Pierre <delreux.pierre@gmail.com>
2019-03-29 15:42:53 +0100

    addProvider Style

--- src/components/addProvider.vue
+++ src/components/addProvider.vue
@@ -1,31 +1,29 @@
 <template>
-  <div id="add-blog">
-    <h2>Ajout d'un nouveau fournisseur</h2>
-    <form v-if="!submitted">
-      <label>Nom de l'entreprise:</label>
-      <input type="text" v-model.lazy="provider.name" required>
-      <label>Description: </label>
-      <textarea v-model.lazy="provider.description"></textarea>
-      <label>Adresse:</label>
-      <input type="text" v-model.lazy="provider.address" required>
-      <label>Numéro de téléphone:</label>
-      <input type="text" v-model.lazy="provider.phone" required>
-      <label>Mail:</label>
-      <input type="text" v-model.lazy="provider.mail" required>
-      <button v-on:click.prevent="post">Ajout d'un fournisseur</button>
+  <div id="add-blog" class="container">
+    <form v-if="!submitted" class="form">
+      <div class="row container_row">
+        <label class="label">Nom du fournisseur :</label>
+        <input class="input row1" type="text" v-model.lazy="provider.name" required>
+      </div>
+      <div class="row container_row">
+        <label class="label">Adresse : </label>
+        <input class="input row2" v-model.lazy="provider.address"></input>
+      </div>
+      <div class="row container_row">
+        <label class="label">Téléphone :</label>
+        <input class="input row3" type="text" v-model.lazy="provider.phone" required>
+      </div>
+      <div class="row container_row">
+        <label class="label">Adresses mail :</label>
+        <input class="input row4" type="text" v-model.lazy="provider.mail" required>
+      </div>
+      <label class="label">Description:</label>
+      <textarea class="textarea" type="text" v-model.lazy="provider.description" required></textarea>
+      <div class="buttons_container">
+        <button type="button" class="btn btn-danger">Annuler</button>
+        <button type="button" class="btn btn-danger" v-on:click.prevent="post">Ajout d'un fournisseur</button>
+      </div>
     </form>
-    <div v-if="submitted">
-      <h3>Vous avez ajouté un fournisseur</h3>
-    </div>
-    <div id="preview">
-      <h3>Preview Fournisseur</h3>
-      <p>Nom de l'entreprise: {{provider.name}}</p>
-      <p>Description:</p>
-      <p>{{provider.description}}</p>
-      <p>Adresse: {{provider.address}}</p>
-      <p>Numéro de téléphone:: {{provider.phone}}</p>
-      <p>Mail: {{provider.mail}}</p>
-    </div>
   </div>
 </template>
 
@@ -55,4 +53,58 @@ export default {
 </script>
 
 <style scoped>
+.row1 {
+  width: 76%;
+}
+.row2 {
+  width: 89%;
+}
+.row3 {
+  width: 86%;
+}
+.row4 {
+  width: 83%;
+}
+.input {
+  border-style: none;
+  color: white;
+  -webkit-appearance: none;
+  background-color: #585858;
+  border-color: #474747;
+  border-radius: 10px;
+}
+.label {
+  margin-right: 10px;
+  color: white;
+}
+.buttons_container {
+  display: flex;
+  justify-content: space-between;
+}
+.textarea {
+  background-color: #585858;
+  border-color: #474747;
+  border-radius: 25px;
+  margin-bottom: 30px;
+  height: 200px;
+}
+.container_row {
+  margin-bottom: 30px;
+}
+.container {
+  background-color: #171717;
+  border-color: #474747;
+  border-radius: 25px;
+  width: 800px;
+}
+.form {
+  padding: 20px 20px 20px 20px;
+  display: flex;
+  flex-direction: column;
+}
+@media screen and (max-width: 991px) {
+  .input{
+    width: 100%;
+  }
+}
 </style>
