8e0347118d84b814cf4f0a6f59a4bfdeb7515a58
Valentin Guibert <valentinguibert@hotmail.fr>
2019-04-02 09:28:30 +0200

    add leaflet in main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import Routes from './routes'
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
 
+Vue.use(LMap);
+Vue.use(LTileLayer);
+Vue.use(LMarker);
 Vue.use(VueResource);
 Vue.use(VueRouter);
 Vue.filter("snippet", function(value) {
